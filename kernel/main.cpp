548598c0ae726700b3b6b236343ee3962d307361
taka1226 <taka2637napoleon815@icloud.com>
2022-02-13 13:32:13 +0900

    Aのフォント が出力できるようになった

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,6 +3,27 @@
 
 #include "frame_buffer_config.hpp"
 
+// #@@range_begin(font_a)
+const uint8_t kFontA[16] = {
+  0b00000000, //
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00011000, //    **
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b00100100, //   *  *
+  0b01111110, //  ******
+  0b01000010, //  *    *
+  0b01000010, //  *    *
+  0b01000010, //  *    *
+  0b11100111, // ***  ***
+  0b00000000, //
+  0b00000000, //
+};
+// #@@range_end(font_a)
+
 struct PixelColor {
     uint8_t r, g, b;
 };
@@ -53,6 +74,19 @@ void* operator new(size_t size, void* buf){
 void operator delete(void* obj) noexcept {
 }
 
+
+void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color){
+    if (c != 'A'){
+        return;
+    }
+    for (int dy=0;dy<16;dy++){
+        for (int dx=0;dx<8;dx++){
+            if ((kFontA[dy] << dx) & 0x80u){
+                writer.Write(x + dx, y + dy, color);
+            }
+        }
+    }
+}
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
@@ -78,5 +112,8 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
             pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
+
+    WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+    WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
     while(1) __asm__("hlt");
 }
