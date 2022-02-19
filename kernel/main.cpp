093aeebe34d6f6a494c3b0fbf1bb52957ed8192c
taka1226 <taka2637napoleon815@icloud.com>
2022-02-19 20:00:29 +0900

    第5章 完了

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -15,7 +15,22 @@ void operator delete(void* obj) noexcept {
 }
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+char console_buf[sizeof(Console)];
 PixelWriter* pixel_writer;
+Console* console;
+
+int printk(const char* format, ...){
+    va_list ap;
+    int result;
+    char s[1024];
+
+    va_start(ap, format);
+    result = vsprintf(s, format, ap);
+    va_end(ap);
+
+    console->PutString(s);
+    return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     switch (frame_buffer_config.pixel_format) {
@@ -34,12 +49,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         }
     }
 
-    Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+    console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-    char buf[128];
-    for (int i=0;i<29;i++){
-        sprintf(buf, "line %d\n", i);
-        console.PutString(buf);
+    for (int i=0;i<27;i++){
+        printk("printk: %d\n", i);
     }
     while(1) __asm__("hlt");
 }
