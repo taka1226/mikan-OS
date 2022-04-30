41a0771226e8ecf702f4fbeb2fb6713eda51558d
taka1226 <taka2637napoleon815@icloud.com>
2022-04-30 15:20:11 +0900

    11 d まで完了

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -97,7 +97,10 @@ extern "C" void KernelMainNewStack(
 
   layer_manager->Draw({{0, 0}, ScreenSize()});
 
-  InitializeLAPICTimer();
+  InitializeLAPICTimer(*main_queue);
+
+  timer_manager->AddTimer(Timer(200, 2));
+  timer_manager->AddTimer(Timer(600, -1));
 
   char str[128];
 
@@ -127,8 +130,11 @@ extern "C" void KernelMainNewStack(
         case Message::kInterruptXHCI:
             usb::xhci::ProcessEvents();
             break;
-        case Message::kInterruptLAPICTimer:
-            printk("Timer interrupt\n");
+        case Message::kTimerTimeout:
+            printk("Timer: timeout = %lu, vaue = %d\n", msg.arg.timer.timeout, msg.arg.timer.value);
+            if (msg.arg.timer.value > 0){
+                timer_manager->AddTimer(Timer(msg.arg.timer.timeout + 100, msg.arg.timer.value + 1));
+            }
             break;
         default:
           Log(kError, "Unknown message type: %d\n", msg.type);
