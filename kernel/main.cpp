dde22ab5e095b7fee380f58c245b5ce9b9796b3a
taka1226 <taka2637napoleon815@icloud.com>
2022-06-14 16:52:59 +0900

    カーソルが点滅するようになった。12章完了

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -85,7 +85,15 @@ void InitializeTextWindow(){
     layer_manager->UpDown(text_window_layer_id, std::numeric_limits<int>::max());
 }
 
+
 int text_window_index;
+
+void DrawTextCursor(bool visible){
+    const auto color = visible ? ToColor(0) : ToColor(0xffffff);
+    const auto pos = Vector2D<int>{8 + 8 * text_window_index, 24 + 5};
+    FillRectangle(*text_window->Writer(), pos, {7, 15}, color);
+}
+
 void InputTextWindow(char c){
     if (c == 0){
         return;
@@ -93,13 +101,17 @@ void InputTextWindow(char c){
 
     auto pos = [](){ return Vector2D<int>{8 + 8 * text_window_index, 24 + 6}; };
 
-    const int max_chars = (text_window->Width() - 16) / 8;
+    const int max_chars = (text_window->Width() - 16) / 8 - 1;
     if (c == '\b' && text_window_index > 0){
+        DrawTextCursor(false);
         --text_window_index;
         FillRectangle(*text_window->Writer(), pos(), {8, 16}, ToColor(0xffffff));
+        DrawTextCursor(true);
     }else if (c >= ' ' && text_window_index < max_chars){
+        DrawTextCursor(false);
         WriteAscii(*text_window->Writer(), pos(), c, ToColor(0));
         ++text_window_index;
+        DrawTextCursor(true);
     }
 
     layer_manager->Draw(text_window_layer_id);
@@ -144,6 +156,13 @@ extern "C" void KernelMainNewStack(
 
   InitializeKeyboard(*main_queue);
 
+  const int kTextboxCursorTimer = 1;
+  const int kTimer05Sec = static_cast<int>(kTimerFreq * 0.5);
+  __asm__("cli");
+  timer_manager->AddTimer(Timer{kTimer05Sec, kTextboxCursorTimer});
+  __asm__("sti");
+  bool textbox_cursor_visible = false;
+
   char str[128];
 
   while (true) {
@@ -173,6 +192,16 @@ extern "C" void KernelMainNewStack(
             usb::xhci::ProcessEvents();
             break;
         case Message::kTimerTimeout:
+            if (msg.arg.timer.value == kTextboxCursorTimer){
+                __asm__("cli");
+                timer_manager->AddTimer(
+                    Timer{msg.arg.timer.timeout + kTimer05Sec, kTextboxCursorTimer}
+                );
+                __asm__("sti");
+                textbox_cursor_visible = !textbox_cursor_visible;
+                DrawTextCursor(textbox_cursor_visible);
+                layer_manager->Draw(text_window_layer_id);
+            }
             break;
         case Message::kKeyPush:
             InputTextWindow(msg.arg.keyboard.ascii);
