537fcb0040595aee5148915502c9dedb5bbc2f92
taka1226 <taka2637napoleon815@icloud.com>
2022-06-20 23:52:08 +0900

    P.313 まで終了した

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -117,6 +117,53 @@ void InputTextWindow(char c){
     layer_manager->Draw(text_window_layer_id);
 }
 
+// #@@range_begin(taskb_window)
+std::shared_ptr<Window> task_b_window;
+unsigned int task_b_window_layer_id;
+void InitializeTaskBWindow(){
+    task_b_window = std::make_shared<Window>(
+        160, 52, screen_config.pixel_format);
+    DrawWindow(*task_b_window->Writer(), "TaskB Window");
+
+    task_b_widow_layer_id = layer_manager->NewLayer()
+        .SetWindow(task_b_window)
+        .SetDraggable(true)
+        .Move({100, 100})
+        .ID();
+
+    layer_manager->UpDown(task_b_window_layer_id, std::numeric_limits<int>::max());
+}
+// #@@range_end(taskb_window)
+
+// #@@range_begin(task_context)
+struct TaskContext {
+    uint64_t cr3, rip, rflags, reserved1; //offset 0x00
+    uint64_t cs, ss, fs, gs; //offset 0x20
+    uint64_t rax, rbx, rcx, rdx, rdi, rsi, rsp, rbp; //offset 0x40
+    uint64_t r8, r9, r10, r11, r12, r13, r14, r15; //offset 0xc0
+} __attribute__((packed));
+
+alignas(16) TaskContext taqsk_b_ctx, task_a_ctx;
+// #@@range_end(task_context)
+
+// #@@range_begin(taskb_func)
+void TaskB(int task_id, int data){
+    printk("TaskB: task_id=%d, data=%d\n", task_id, data);
+    char str[128];
+    int count = 0;
+    while (true){
+        ++count;
+        sprintf(str, "%010d", count);
+        FillRectangle(*task_b_window->Writer(), {24, 28}, {8 * 10, 16}, {0xc6, 0xc6, 0xc6});
+        WriteString(*task_b_window->Writer(), {24, 28}, str, {0, 0, 0});
+        layer_manager->Draw(task_b_widow_layer_id);
+
+        SwitchContext(&task_a_ctx, &task_b_ctx);
+    }
+}
+// #@@range_end(taskb_func)
+
+
 std::deque<Message>* main_queue;
 
 alignas(16) uint8_t kernel_main_stack[1024 * 1024];
